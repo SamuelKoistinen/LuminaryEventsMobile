693a9b1b4ba22c3ccbdbe534ef58c657772f6111
Tino Roivanen <50320109+Tinosaurus@users.noreply.github.com>
2024-07-11 14:05:58 +0300

    Added navbar widget + the demo of it.
    Now just to trim things down, and then actually start porting over the features we need.

--- luminary_events/android/app/src/main/kotlin/com/example/luminary_events/MainActivity.kt
+++ luminary_events/android/app/src/main/kotlin/com/example/luminary_events/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.luminary_events
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- luminary_events/lib/main.dart
+++ luminary_events/lib/main.dart
@@ -1,137 +1,86 @@
-import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
-import 'package:table_calendar/table_calendar.dart';
+import "package:luminary_events/interactive_example.dart";
+import "package:luminary_events/screens.dart";
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart";
 
-void main() =>
-    runApp(const MaterialApp(
-    home:NavBars(),
-    debugShowCheckedModeBanner: false,
-  
-  ));
-class NavBars extends StatefulWidget {
-  const NavBars({super.key});
-
-  @override
-  _NavBarsState createState() => _NavBarsState();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // make navigation bar transparent
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  // make flutter draw behind navigation bar
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  runApp(const PersistenBottomNavBarDemo());
 }
 
-class _NavBarsState extends State<NavBars> {
-PersistentTabController _controller = PersistentTabController(initialIndex: 0);
-
-    List<PersistentBottomNavBarItem> _NavBarItems() {
-      return [
-        PersistentBottomNavBarItem(
-          icon: const Icon(Icons.home),
-          title: "Home",
-          activeColorPrimary: Colors.blue,
-          inactiveColorPrimary: Colors.grey,
-        ),
-         PersistentBottomNavBarItem(
-          icon: const Icon(Icons.warehouse),
-          title: "Varasto",
-          activeColorPrimary: Colors.red,
-          inactiveColorPrimary: Colors.grey,
-        ),
-         PersistentBottomNavBarItem(
-          icon: const Icon(Icons.calendar_month_outlined),
-          title: "Kalenteri",
-          activeColorPrimary: Colors.green,
-          inactiveColorPrimary: Colors.grey,
-        ), 
-      ];
-    }
-  
-   List<Widget> _screens() {
-    return [
-      HomeScreen(),
-      VarastoScreen(),
-      KalenteriScreen(),
-    ];
-   }
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          body: PersistentTabView (
-          context,
-          controller: _controller,
-          screens: _screens(),
-          items: _NavBarItems(),
-          confineInSafeArea: true,
-          )
-          
-        );
-      }
-}
-
-class KalenteriScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return (_MyAppState().calendarcontent());
-
-  }
-}
-class VarastoScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text("VarastoScreen"));
-  }
-}
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text("HomeScreen"));
-  }
-}
-
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class PersistenBottomNavBarDemo extends StatelessWidget {
+  const PersistenBottomNavBarDemo({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-class _MyAppState extends State<MyApp> {
-
-  DateTime today = DateTime.now();
-  void _OnDaySelected(DateTime day,DateTime focusedDay) {
-    setState(() {
-      today = day;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("HomePage")),
-      body: calendarcontent(),
-    );
-  }
-  Widget calendarcontent() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-           Text("Selected Day = " + today.toString().split(" ")[0]),
-           const Text("Kalenteri"),
-           Container(child: TableCalendar(
-            locale: "en_US",
-            rowHeight: 43,
-            headerStyle: const HeaderStyle(formatButtonVisible: false, titleCentered: true),
-            availableGestures: AvailableGestures.all,
-            selectedDayPredicate: (day) => isSameDay(day, today),
-            focusedDay: today,
-            firstDay: DateTime.utc(2010,16,10),
-            lastDay: DateTime.utc(2030,3,14),
-            onDaySelected: _OnDaySelected,
-            
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Persistent Bottom Navigation Bar Demo",
+        home: Builder(
+          builder: (context) => Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pushNamed("/minimal"),
+                  child: const Text("Show Minimal Example"),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed("/interactive"),
+                  child: const Text("Show Interactive Example"),
+                ),
+              ],
             ),
-          )
-
-        ],
-      ),
-    );
-  }
+          ),
+        ),
+        routes: {
+          "/minimal": (context) => const MinimalExample(),
+          "/interactive": (context) => const InteractiveExample(),
+        },
+      );
 }
- 
-    
-  
 
+class MinimalExample extends StatelessWidget {
+  const MinimalExample({super.key});
+
+  List<PersistentTabConfig> _tabs() => [
+        PersistentTabConfig(
+          screen: const MainScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.home),
+            title: "Etusivu",
+          ),
+        ),
+        PersistentTabConfig(
+          screen: const MainScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.calendar_month_outlined),
+            title: "Kalenteri",
+          ),
+        ),
+        PersistentTabConfig(
+          screen: const MainScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.warehouse),
+            title: "Varasto",
+          ),
+        ),
+      ];
+
+  @override
+  Widget build(BuildContext context) => PersistentTabView(
+        tabs: _tabs(),
+        navBarBuilder: (navBarConfig) => Style1BottomNavBar(
+          navBarConfig: navBarConfig,
+        ),
+      );
+}
