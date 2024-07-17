f11a55310e3b08bf4dceead83afc640d7759ddeb
onde <onni.loponen@gmail.com>
2024-07-17 11:10:59 +0300

    Kalenteripohja
    lisätty kalenteripohja kalenterisivulle, sekä tarvittava tukitiedosto.

--- luminary_events/lib/screens.dart
+++ luminary_events/lib/screens.dart
@@ -1,6 +1,9 @@
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 import "package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart";
+import 'package:table_calendar/table_calendar.dart';
+
+import '../utils.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key, this.useRouter = false});
@@ -9,7 +12,7 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text("Main Screen")),
+        appBar: AppBar(title: const Text("Etusivu")),
         backgroundColor: Colors.indigo,
         body: ListView(
           padding: const EdgeInsets.all(16)
@@ -98,56 +101,64 @@ class MainScreen extends StatelessWidget {
       );
 }
 
-class MainScreen2 extends StatelessWidget {
-  const MainScreen2({super.key, this.useRouter = false});
+//   -----------------------------------------------------
+//  |             kalenterinäkymä on tässä.               |
+//   -----------------------------------------------------
 
+//    Kalenterissa ei funktionaalisuutta, statejen kanssa pitäs värkätä ja
+//    ehkä tehä tästä oma komponentti mutta en tiiä, pitää miettiä ja tutkii.
+
+class MainScreen2 extends StatefulWidget {
+  @override
+  _MainScreen2State createState() => _MainScreen2State();
+
+  const MainScreen2({super.key, this.useRouter = false});
   final bool useRouter;
+}
+
+class _MainScreen2State extends State<MainScreen2> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text("Secondary Screen")),
+        appBar: AppBar(title: const Text("Tapahtumakalenteri")),
         backgroundColor: Colors.teal,
-        body: ListView(
-          padding: const EdgeInsets.all(16)
-              .copyWith(bottom: MediaQuery.of(context).padding.bottom),
-          children: <Widget>[
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if (useRouter) {
-                    context.go("/home/detail/super-detail");
-                  } else {
-                    pushScreen(
-                      context,
-                      screen: const MainScreen3(),
-                      withNavBar: true,
-                    );
-                  }
-                },
-                child: const Text("Popup Screen (Tetriary)"),
-              ),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if (useRouter) {
-                    context.go("/detail/super-detail");
-                  } else {
-                    pushScreen(context, screen: const MainScreen3());
-                  }
-                },
-                child: const Text("Go to Second Screen without Navbar"),
-              ),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Go Back to First Screen"),
-              ),
-            ),
-          ],
+        body: TableCalendar(
+          firstDay: kFirstDay,
+          lastDay: kLastDay,
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          selectedDayPredicate: (day) {
+            // Use `selectedDayPredicate` to determine which day is currently selected.
+            // If this returns true, then `day` will be marked as selected.
+
+            // Using `isSameDay` is recommended to disregard
+            // the time-part of compared DateTime objects.
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              // Call `setState()` when updating the selected day
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              // Call `setState()` when updating calendar format
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          onPageChanged: (focusedDay) {
+            // No need to call `setState()` here
+            _focusedDay = focusedDay;
+          },
         ),
       );
 }
@@ -157,7 +168,7 @@ class MainScreen3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text("Third Screen")),
+        appBar: AppBar(title: const Text("Varasto")),
         backgroundColor: Colors.deepOrangeAccent,
         body: Center(
           child: ElevatedButton(
