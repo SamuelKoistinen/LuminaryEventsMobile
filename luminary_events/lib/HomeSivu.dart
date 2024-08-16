c2da5c4652eeaf407b45c06b33a2891539c02a8e
Tino Roivanen <50320109+Tinosaurus@users.noreply.github.com>
2024-08-16 14:37:54 +0300

    very initial poc of fetching the event data from utils file

--- luminary_events/lib/HomeSivu.dart
+++ luminary_events/lib/HomeSivu.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:luminary_events/CalendarSivu.dart';
-import 'utils.dart';
-
-final List<String> entries = <String>['Haloo', 'Miuku Mauku', '59258'];
-final List<int> colorCodes = <int>[200, 100, 50];
+import 'utils.dart' as utils;
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key, this.useRouter = false});
@@ -14,12 +10,12 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.separated(
       padding: const EdgeInsets.only(top: 30, right: 8, left: 8, bottom: 8),
-      itemCount: entries.length,
+      itemCount: 4,
       itemBuilder: (BuildContext context, int index) {
         return Container(
           height: 50,
-          color: Colors.blue[colorCodes[index]],
-          child: Center(child: Text('Event: ${entries[index]}')),
+          color: Colors.blue,
+          child: Center(child: Text('Event: ${utils.kLastDay}')),
         );
       },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
